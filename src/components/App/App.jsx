28afc41f5e9100ec9b6b797e9e651dc629fc84ba
intakie <intakiea@gmail.com>
2024-04-30 16:47:50 +0300

    fixed css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import TransactionHistory from '../TransactionHistory/TransactionHistory';
 import userData from '../userData.json';
 import friends from '../friends.json';
 import transactions from '../transactions.json';
-import css from './App.module.css';
+// import css from './App.module.css';
 // console.log(css);
 
 export default function App() {
